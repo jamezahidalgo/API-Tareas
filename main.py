46aa3b58db9bb0247e23f9f308081934b58e93b6
jamezahidalgo <ja.meza@profesor.duoc.cl>
2023-09-24 01:01:11 +0000

    Versión 1.1

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ def index():
 
 
 # Blueprint
-#app.register_blueprint(Empleado.main, url_prefix='/api/empleados')
 app.register_blueprint(Usuario.main, url_prefix='/api/usuarios')
 app.register_blueprint(Tarea.main, url_prefix='/api/tareas')
 app.run(host='0.0.0.0', port=81)
